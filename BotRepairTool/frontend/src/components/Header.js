11e0ebe203d6e12ce4af54f072cecfd5566f5c98
ntambara-owen <ntambaraowen75@gmail.com>
2024-06-08 11:58:09 +0200

    Create Header.js

---
+++ BotRepairTool/frontend/src/components/Header.js
@@ -0,0 +1,14 @@
+// frontend/src/components/Header.js
+
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Header Component</h1>
+      {/* Add your header content here */}
+    </header>
+  );
+};
+
+export default Header;
