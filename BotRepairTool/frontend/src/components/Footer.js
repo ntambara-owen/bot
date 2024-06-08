43f663292aab8ef1996605c22349eb6ef5daa3b6
ntambara-owen <ntambaraowen75@gmail.com>
2024-06-08 11:58:36 +0200

    Create Footer.js

---
+++ BotRepairTool/frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+// frontend/src/components/Footer.js
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Footer Component</p>
+      {/* Add your footer content here */}
+    </footer>
+  );
+};
+
+export default Footer;
